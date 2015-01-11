143b6380b3af2c3a8dcffd8fbf63fb973e245f16
zeyuanxy <zeyuanxy@gmail.com>
2015-01-11 21:51:16 +0800

    DeHaze is finished perfectly. Now I need to write the report for DeHaze and GrabCut

--- dehaze/src/softmatting.cpp
+++ dehaze/src/softmatting.cpp
@@ -2,6 +2,8 @@
 #include "include/Eigen/Sparse"
 #include "include/Eigen/LU"
 #include <unordered_map>
+#include <cassert>
+#include <iostream>
 
 using namespace std;
 using namespace cv;
@@ -13,17 +15,17 @@ const double epsilon = 0.0001;
 
 struct Window {
     int size;
-    Vector3f mean;
-    Matrix3f cov;
+    Vector3d mean;
+    Matrix3d cov;
     Window() : size(0) {
         mean << 0, 0, 0;
         cov << 0, 0, 0, 0, 0, 0, 0, 0, 0;
     }
 };
 
-static Vector3f Vec3b2Vector3f(Vec3b color) {
+static Vector3d Vec3b2Vector3d(Vec3b color) {
     double a = color[0], b = color[1], c = color[2];
-    return Vector3f(a, b, c);
+    return Vector3d(a, b, c);
 }
 
 void soft_matting(const Mat &input, const Mat &transmission, Mat &refinedTransmission) {
@@ -40,13 +42,13 @@ void soft_matting(const Mat &input, const Mat &transmission, Mat &refinedTransmi
                 for (int y = j - windowSize / 2; y <= j + windowSize / 2; ++y)
                     if (x >= 0 && x < input.rows && y >= 0 && y < input.cols) {
                         ++window.size;
-                        window.mean += Vec3b2Vector3f(input.at<Vec3b>(x, y));
+                        window.mean += Vec3b2Vector3d(input.at<Vec3b>(x, y));
                     }
             window.mean /= window.size;
             for (int x = i - windowSize / 2; x <= i + windowSize / 2; ++x)
                 for (int y = j - windowSize / 2; y <= j + windowSize / 2; ++y)
                     if (x >= 0 && x < input.rows && y >= 0 && y < input.cols) {
-                        Vector3f diff = Vec3b2Vector3f(input.at<Vec3b>(x, y)) - window.mean;
+                        Vector3d diff = Vec3b2Vector3d(input.at<Vec3b>(x, y)) - window.mean;
                         window.cov += diff * diff.transpose();
                     }
             window.cov /= window.size - 1;
@@ -55,12 +57,12 @@ void soft_matting(const Mat &input, const Mat &transmission, Mat &refinedTransmi
     
     // calculate Matting Laplacian matrix L
     printf("calculate Matting Laplacian matrix L ...\n");
-    Matrix3f U3;
+    Matrix3d U3;
     U3 << 1, 0, 0, 0, 1, 0, 0, 0, 1;
     vector<Triplet<double>> listL;
     for (int i = 0; i < totalPxl; ++i) {
         int row = i / input.cols, col = i % input.cols;
-        Vector3f Ii = Vec3b2Vector3f(input.at<Vec3b>(row, col));
+        Vector3d Ii = Vec3b2Vector3d(input.at<Vec3b>(row, col));
         unordered_map<int, double> ele;
         for (int x = row - windowSize / 2; x <= row + windowSize / 2; ++x)
             for (int y = col - windowSize / 2; y <= col + windowSize / 2; ++y)
@@ -69,10 +71,10 @@ void soft_matting(const Mat &input, const Mat &transmission, Mat &refinedTransmi
                         for (int jy = y - windowSize / 2; jy <= y + windowSize / 2; ++jy)
                             if (jx >= 0 && jx < input.rows && jy >= 0 && jy < input.cols) {
                                 int j = jx * input.cols + jy;
-                                Vector3f Ij = Vec3b2Vector3f(input.at<Vec3b>(jx, jy));
-                                Vector3f left = Ii - windows[x * input.cols + y].mean;
-                                Matrix3f middle = windows[x * input.cols + y].cov + epsilon / windows[x * input.cols + y].size * U3;
-                                Vector3f right = Ij - windows[x * input.cols + y].mean;
+                                Vector3d Ij = Vec3b2Vector3d(input.at<Vec3b>(jx, jy));
+                                Vector3d left = Ii - windows[x * input.cols + y].mean;
+                                Matrix3d middle = windows[x * input.cols + y].cov + epsilon / windows[x * input.cols + y].size * U3;
+                                Vector3d right = Ij - windows[x * input.cols + y].mean;
                                 ele[j] += (i == j) - 1.0 / windows[x * input.cols + y].size * (1.0 + left.transpose() * middle.inverse() * right);
                             }
                 }
@@ -116,6 +118,9 @@ void soft_matting(const Mat &input, const Mat &transmission, Mat &refinedTransmi
     // set the answer back to refinedTransmission
     printf("set the answer back to refinedTransmission ...\n");
     for (int i = 0; i < input.rows; ++i)
-        for (int j = 0; j < input.cols; ++j)
+        for (int j = 0; j < input.cols; ++j) {
+            if (isnan(t[i * input.cols + j]))
+                continue;
             refinedTransmission.at<double>(i, j) = t[i * input.cols + j];
+        }
 }
